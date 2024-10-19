59f9086f58c06cc42146c215e55ddadb8340d921
Dmitry Naumenko <dimanaumenko31@gmail.com>
2024-10-19 16:36:02 +0300

    add

--- src/2-form.js
+++ src/2-form.js
@@ -7,6 +7,12 @@ let formData = {
  
 const form = document.querySelector('.feedback-form');
 
+function saveToLocalStorage() {
+  formData.email = form.email.value.trim();
+  formData.message = form.message.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const savedData = localStorage.getItem('feedback-form-state');
     if (savedData) {
@@ -17,19 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 form.addEventListener('input', (event) => {
-    formData[event.target.name] = event.target.value;
+    formData[event.target.name] = event.target.value.trim();
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-        
-    if (formData.email === "" || formData.message === "") {
+
+    if (form.email.value.trim() === "" || form.message.value.trim() === "") {
         alert("Fill please all fields");
         return;
     }
 
-    console.log(formData);
+    console.log({
+        email: form.email.value.trim(),
+        message: form.message.value.trim()
+    });
 
     localStorage.removeItem('feedback-form-state');
     formData = { email: "", message: "" };
